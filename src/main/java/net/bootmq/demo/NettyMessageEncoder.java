8d31d27c6b48bab1661f0e9099f377b8b7160ead
xuanhua.hu <xuanhua.hu@busonline.com>
2017-04-11 20:49:40 +0800

    添加了netty5 socket的文件传送的server部分

---
+++ src/main/java/net/bootmq/demo/NettyMessageEncoder.java
@@ -0,0 +1,22 @@
+package net.bootmq.demo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+
+/**
+ * 编码器
+ * @author 洋白菜
+ *
+ */
+public class NettyMessageEncoder extends MessageToMessageEncoder<Object> {
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, Object msg,
+			List<Object> out) throws Exception {
+		out.add(ObjectConvertUtil.request(msg));
+	}
+
+}
